2f1d8832cb6caaad6b54081d537d78b57224911d
Siddharth Jain <jainnsidhearth77@gmail.com>
2026-05-23 07:00:53 +0530

    Feat: 5-setup-frontend-react-framework

--- octofit-tracker/frontend/src/App.tsx
+++ octofit-tracker/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
 import Activities from "./components/Activities";
--- octofit-tracker/frontend/src/components/Teams.tsx
+++ octofit-tracker/frontend/src/components/Teams.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { buildApiUrl, normalizeResponse } from "../api";
 
 function Teams() {
